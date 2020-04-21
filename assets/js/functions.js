737a629aa6248a8a964aa3e9874dfa7209853f34
Tochukwu  Franklin Ene <teec3000@gmail.com>
2020-04-21 16:05:12 +0200

    Added countdown timer to the index page

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -148,16 +148,35 @@
 
     /* ------------------ COUNTDOWN DATE ------------------ */
 
-    $(".countdown").each(function() {
-        var $countDown = $(this),
-            countDate = $countDown.data("count-date"),
-            newDate = new Date(countDate);
-        $countDown.countdown({
-            until: newDate,
-            format: "MMMM Do , h:mm:ss a"
-        });
-    });
 
+$(".countDownDate").each(function(){
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  var t = countDownDate - now; 
+var days = Math.floor(t / (1000 * 60 * 60 * 24)); 
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60)); 
+var seconds = Math.floor((t % (1000 * 60)) / 1000); 
+document.getElementById("day").innerHTML =days ; 
+document.getElementById("hour").innerHTML =hours; 
+document.getElementById("minute").innerHTML = minutes; 
+document.getElementById("second").innerHTML =seconds; 
+if (t < 0) { 
+        clearInterval(x); 
+        document.getElementById("demo").innerHTML = "TIME UP"; 
+        document.getElementById("day").innerHTML ='0'; 
+        document.getElementById("hour").innerHTML ='0'; 
+        document.getElementById("minute").innerHTML ='0' ; 
+        document.getElementById("second").innerHTML = '0'; } 
+}, 1000); 
+});
     /* ------------------  AJAX MAILCHIMP ------------------ */
 
     $('.mailchimp').ajaxChimp({
